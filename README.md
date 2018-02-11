9212f368e7c646c91abcd52bd968a5a11f19c4eb
Pankaj Kumar Singh <pankaj28843@gmail.com>
2018-02-11 10:51:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HYF-CPH-Week-6
---
+++ REVIEW.js
@@ -0,0 +1,21 @@
+function fetchJsonData(url, callBackFunction) {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', function() {
+        callBackFunction(JSON.parse(request.responseText));
+    });
+
+    request.open('get', url);
+    request.send();
+}
+
+function loadMovies() {
+    const url = 'https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json';
+    const movies = []
+    fetchJsonData(url, function(data) {
+        movies = data;
+
+        console.log('Loaded', movies);
+
+    });
+}
+
