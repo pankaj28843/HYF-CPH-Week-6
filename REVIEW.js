7c72d109c506df883f8b830ad35c5b0911295edc
Pankaj Kumar Singh <pankaj28843@gmail.com>
2018-02-11 10:52:47 +0100

    fix: assignment to const is wrong

--- REVIEW.js
+++ REVIEW.js
@@ -10,7 +10,7 @@ function fetchJsonData(url, callBackFunction) {
 
 function loadMovies() {
     const url = 'https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json';
-    const movies = []
+    let movies = []
     fetchJsonData(url, function(data) {
         movies = data;
 
@@ -19,3 +19,4 @@ function loadMovies() {
     });
 }
 
+loadMovies();
